606910c562522458236801f1dd81c12d5fa8a572
安小游 <471607346@qq.com>
2019-06-11 17:03:23 +0800

    commit

--- resources/views/pai/home/began.blade.php
+++ resources/views/pai/home/began.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="format-detection" content="telephone=yes"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
     <title>安全隐患随手拍</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
