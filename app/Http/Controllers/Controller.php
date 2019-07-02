3e7750ba6fbf7728ab5eff37698e19c173419ed7
安小游 <471607346@qq.com>
2019-07-02 10:21:53 +0800

    commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Doctrine\Common\Cache\Cache;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class Controller extends BaseController
 {
